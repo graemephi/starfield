668ce89996b9b76af345748011f481d4b88b6204
graeme <graemephi@gmail.com>
2019-02-21 00:53:11 +0000

    depersonalise build

--- build.bat
+++ build.bat
@@ -1,14 +1,18 @@
 @echo off
 
+REM
+REM %Starfield_SDL%: Location of SDL
+REM %Starfield_Ion$: Location of ion executable
+REM
+
 REM VSCodes no good sometimes
 if "%1" == "clear" (
     clear
     clear
 )
 
-set BaseInclude=C:\code\include
-set SDLInclude=%BaseInclude%\SDL2\include
-set SDLLib=%BaseInclude%\SDL2\lib\x64\SDL2.lib %BaseInclude%\SDL2\lib\x64\SDL2main.lib
+set SDLInclude=%Starfield_SDL%\include
+set SDLLib=%Starfield_SDL%\lib\x64\SDL2.lib %Starfield_SDL%\lib\x64\SDL2main.lib
 
 set LinkerOptions=-subsystem:windows
 
@@ -16,12 +20,12 @@ set DOptimisationLevel=-Od -Ob1
 set OptimisationLevel=-Ox -Ob2
 set CompilerOptions=-Zi -Oi %OptimisationLevel% -W3 -WX -nologo -fp:fast -MTd
 
-call C:\code\cloned\bitwise\ion.exe starfield
+call %Starfield_Ion% starfield
 if errorlevel 1 goto end
 
 mkdir build 2> nul
 pushd build
-call cl.exe %CompilerOptions% -I%SDLInclude% -I%BaseInclude% -Fe:starfield.exe ..\out_starfield.c %SDLLib% -link %LinkerOptions%
+call cl.exe %CompilerOptions% -I%SDLInclude% -Fe:starfield.exe ..\out_starfield.c %SDLLib% -link %LinkerOptions%
 popd
 
 :end
