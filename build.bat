968bfed4019b95408623fba8d80a589b277af24b
graeme <graemephi@gmail.com>
2019-02-22 22:31:29 +0000

    support clang-cl, cleanup, no shirking on alignment
    also fix a real embarrassing bug!!! too enamoured with &{} init probably

--- build.bat
+++ build.bat
@@ -16,16 +16,19 @@ set SDLLib=%Starfield_SDL%\lib\x64\SDL2.lib %Starfield_SDL%\lib\x64\SDL2main.lib
 
 set LinkerOptions=-subsystem:windows
 
-set DOptimisationLevel=-Od -Ob1
-set OptimisationLevel=-Ox -Ob2
-set CompilerOptions=-Zi -Oi %OptimisationLevel% -W3 -WX -nologo -fp:fast -MTd
+set DOptimisationLevel=-Od -Ob1 -MTd
+set OptimisationLevel=-Ox -Ob2 -MT
+set MSVCOptions=-W3 -WX
+set ClangOptions=-mfma -W0 -arch:AVX2
+set CompilerOptions=-Zi -Oi %OptimisationLevel% -nologo -fp:fast
 
-call %Starfield_Ion% starfield
+call %Starfield_Ion% -notypeinfo starfield
 if errorlevel 1 goto end
 
 mkdir build 2> nul
 pushd build
-call cl.exe %CompilerOptions% -I%SDLInclude% -Fe:starfield.exe ..\out_starfield.c %SDLLib% -link %LinkerOptions%
+REM call cl %MSVCOptions% %CompilerOptions% -I%SDLInclude% -Festarfield.exe ..\out_starfield.c %SDLLib% -link %LinkerOptions%
+call clang-cl %ClangOptions% %CompilerOptions% -I%SDLInclude% -Festarfield.exe ..\out_starfield.c %SDLLib% -link %LinkerOptions%
 popd
 
 :end
