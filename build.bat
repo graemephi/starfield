7539c57843b97ff7c7647c2bd80f02f2b6828dde
graeme <graemephi@gmail.com>
2019-02-27 00:26:41 +0000

    simd worley

--- build.bat
+++ build.bat
@@ -14,21 +14,21 @@ if "%1" == "clear" (
 set SDLInclude=%Starfield_SDL%\include
 set SDLLib=%Starfield_SDL%\lib\x64\SDL2.lib %Starfield_SDL%\lib\x64\SDL2main.lib
 
-set LinkerOptions=-subsystem:windows
+set LinkerOptions=-subsystem:windows /debug:fastlink
 
-set DOptimisationLevel=-Od -Ob1 -MTd
-set OptimisationLevel=-Ox -Ob2 -MT
+set NOpt=-Od -Ob1 -MT
+set Opt=-Ox -Ob2 -MT
 set MSVCOptions=-W3 -WX
 set ClangOptions=-mfma -W0 -arch:AVX2
-set CompilerOptions=-Zi -Oi %OptimisationLevel% -nologo -fp:fast
+set CompilerOptions=-Zi -Oi %Opt% -nologo -fp:fast
 
 call %Starfield_Ion% -notypeinfo starfield
 if errorlevel 1 goto end
 
 mkdir build 2> nul
 pushd build
-REM call cl %MSVCOptions% %CompilerOptions% -I%SDLInclude% -Festarfield.exe ..\out_starfield.c %SDLLib% -link %LinkerOptions%
-call clang-cl %ClangOptions% %CompilerOptions% -I%SDLInclude% -Festarfield.exe ..\out_starfield.c %SDLLib% -link %LinkerOptions%
+call cl %MSVCOptions% %CompilerOptions% -I%SDLInclude% -Festarfield.exe ..\out_starfield.c %SDLLib% -link %LinkerOptions%
+REM call clang-cl %ClangOptions% %CompilerOptions% -I%SDLInclude% -Festarfield.exe ..\out_starfield.c %SDLLib% -link %LinkerOptions%
 popd
 
 :end
