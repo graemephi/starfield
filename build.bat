08486f2f34eb68c8fa93e0d53e654709f60b8757
graeme <graemephi@gmail.com>
2020-02-22 00:51:07 +0000

    cleanup build.bat

--- build.bat
+++ build.bat
@@ -13,7 +13,6 @@ set LinkerOptions=-subsystem:windows
 set NOpt=-Od -Ob1 -MTd
 set Opt=-O2 -GL -Ob2 -MT
 set MSVCOptions=-W3 -WX -wd4101
-set ClangOptions=-mfma -W0 -arch:AVX2
 set CompilerOptions=-Zi -Oi %Opt% -nologo -fp:fast
 
 call %Starfield_Ion% -notypeinfo starfield
@@ -22,9 +21,6 @@ if errorlevel 1 goto end
 mkdir build 2> nul
 pushd build
 call cl %MSVCOptions% %CompilerOptions% -I%SDLInclude% -Festarfield.exe ..\out_starfield.c %SDLLib% -link %LinkerOptions%
-REM call clang-cl %ClangOptions% %CompilerOptions% -I%SDLInclude% -Festarfield.exe ..\out_starfield.c %SDLLib% -link %LinkerOptions%
 popd
 
 :end
-
-REM clang -mavx -mxsave -mfma -mlzcnt -c out_starfield.c
